4af1dfe77ea65fc3fc9b1ef10e39e4051c053be0
alpo <leesb7755@yahoo.co.kr>
2018-07-12 12:01:40 +0900

    MyHomePage - 0001

---
+++ src/main/java/kr/green/springwebproject/dao/UserMapper.java
@@ -0,0 +1,26 @@
+package kr.green.springwebproject.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.green.springwebproject.pagenation.Criteria;
+
+public interface UserMapper {
+	
+	public User login(
+		@Param("id") String id,@Param("pw") String pw);
+	public void signup(@Param("user")User user);
+	
+	public User loginById(@Param("id")String id);
+	
+	public void updateUser(@Param("user")User user);
+	
+	public List<User> userListExceptLoginUser(@Param("user")User user, 
+			@Param("cri")Criteria cri);
+	
+	public int countUsersExceptLoginUser(@Param("user")User user);
+	
+	public void withdraw(@Param("user")User user);
+
+}
